eb2016e0aeb05cd14b94f84fcf6714c71096744a
le-gucci <lucy.ebben@gmail.com>
2026-06-23 11:00:47 -0500

    pi safety

--- vision.py
+++ vision.py
@@ -1,14 +1,21 @@
 import cv2 as cv
+from picamera2 import Picamera2
 import numpy as np
 
-camfeed = cv.VideoCapture(0)
+picam2 = Picamera2()
+
+config = picam2.create_preview_configuration(main={"size": (640, 480)})
+picam2.configure(config)
+picam2.start()
+
+## camfeed = cv.VideoCapture(0)
 
 while True:
-    isTrue, frame = camfeed.read()
+    frame = picam2.capture_array()
 
-    print(isTrue)
+    ##print(isTrue)
 
-    ir = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    ir = cv.cvtColor(frame, cv.COLOR_RGB2HSV)
 
     lower_bound = np.array([130, 50, 50])
     upper_bound = np.array([180, 255, 255])
@@ -33,8 +40,9 @@ while True:
 
 
     cv.imshow("view", frame)
-    if cv.waitKey(1) & 0xFF == ord('q'):
+
+    if cv.waitKey(1) == ord('q'):
         break
 
-cap.release()
 cv.destroyAllWindows()
+picam2.stop()
