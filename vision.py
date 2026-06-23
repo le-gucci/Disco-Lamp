8f388e1c9a349773e38addbff9f89bc19588e305
le-gucci <lucy.ebben@gmail.com>
2026-06-23 08:39:10 -0500

    print_coords

--- vision.py
+++ vision.py
@@ -3,33 +3,36 @@ import numpy as np
 
 camfeed = cv.VideoCapture(0)
 
-
 while True:
     isTrue, frame = camfeed.read()
 
+    ir = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
+    lower_bound = np.array([130, 50, 50])
+    upper_bound = np.array([180, 255, 255])
 
-    ## create mask to find ir
-
-    ## ir = cv.cvtColor(
-
-    ## lower_bound
-    ## upper_bound
-
-    ## irmask = cv.inRange
-    irmask = None
-
+    irmask = cv.inRange(ir, lower_bound, upper_bound)
 
     contours, _ = cv.findContours(irmask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
-    irblob = max(contours, key = cv.contourArea)
+    if contours:
+        irblob = max(contours, key = cv.contourArea)
 
-    M = cv.moments(irblob)
+        cv.drawContours(frame, [irblob], 0, (0, 0, 0), 1)
 
-    cx = M["m10"]/M["m00"]
-    cy = M["m01"]/M["m00"]
+        M = cv.moments(irblob)
 
-    cv.circle(frame, (cx, cy), 5, (0, 255, 0), 2)
+        if M["m00"] != 0:
+            cx = int(M["m10"]/M["m00"])
+            cy = int(M["m01"]/M["m00"])
+
+            cv.circle(frame, (cx, cy), 5, (0, 255, 0), 2)
+            print(cx, cy)
 
 
-    cv.imshow(frame)
+    cv.imshow("view", frame)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
